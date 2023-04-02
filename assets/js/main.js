3750db81ed8e7851e877b17d17dafa9e08ef45d7
Amit Kushwaha <akkushwaha018@gmail.com>
2023-04-02 22:00:20 +0530

    Responsive work and buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,3 +101,97 @@ $('.premium-div-button').click(function(){
         $('#exclusive_amount').val(totalPriceExclusive);
     }
 });
+var fileobj;
+    function upload_file(e) {
+        e.preventDefault();
+        ajax_file_upload(e.dataTransfer.files);
+    }
+
+    function file_explorer() {
+        document.getElementById('selectfile').click();
+        document.getElementById('selectfile').onchange = function() {
+            files = document.getElementById('selectfile').files;
+            ajax_file_upload(files);
+        };
+    }
+
+    function ajax_file_upload(file_obj) {
+        if(file_obj != undefined) {
+            var formdata = new FormData();
+            formdata.append('_token', 'wvmZC3m8J6rlKlKPclwWpK0PWNQodVRSAO3gBNbm');
+            formdata.append('keys', 'in6831b799758135f1a14df0f5470c264c');
+            $('#loading').css('display', 'block');
+
+            for(i=0; i<=file_obj.length; i++) {
+                formdata.append('file[]', file_obj[i]);
+            }
+            $.ajax({
+                type: 'POST',
+                url: baseUrl+'/ajax_image_upload',
+                contentType: false,
+                processData: false,
+                data: formdata,
+                beforeSend:function(){
+                    $('#success').empty();
+                    $('.progress-bar').text('0%');
+                    $('.progress-bar').css('width', '0%');
+                },
+                uploadProgress:function(event, position, total, percentComplete){
+                    $('.progress-bar').text(percentComplete + '0%');
+                    $('.progress-bar').css('width', percentComplete + '0%');
+                },
+                success:function(data) {
+                    if(data.files) {
+                            var myHtml = "";
+                            data.files.forEach(function(item,key) {
+                                myHtml +='<div class="img-upload-grid remove-img-grid" id="proimg_'+item[0]+'"> <div class="up-img-inner-box img-inserted"><label class="card w-100 mb-0"><input  type="hidden" name=""><div class="img-preview-label no-drag" profile-id="'+item[0]+'"></div><div class="image-overlay-item-wrap"><span class="delete-img" data-id="'+item[0]+'"><i class="fa fa-times" aria-hidden="true"></i></span><span class="rotate-img" onclick="rotateElem('+item[0]+')"><i class="fa fa-undo" aria-hidden="true"></i></div><img src="'+item[1]+'" class="uploaded-img pic-view_'+item[0]+'" image-id="'+item[0]+'" id="pic-view"></label></div></div>';
+                             });
+                            $('.remove-img-grid').remove();
+                            $('#img-drag-and-drop').prepend(myHtml);
+                            $('input[name="profile_images_id"]').attr('value',data.images_ids);
+                          } //if files get
+                      $('#loading').css('display', 'none');
+                    },
+                    error: function (xhr, status, error) {
+                       if(status=='error'){
+                           alert("Image(s) couldn't be saved. Please check if you are trying to save more than 10 images or other than JPEG,JPG, PING or the images more than 3MB.")
+                       }else{
+                           alert(xhr.responseText);
+                       }
+                    $('#loading').css('display', 'none');
+                    }
+            });
+        }
+    }
+
+    $(document).on("click", "span.delete-img" , function() {
+            var token = $('meta[name="csrf-token"]').attr('content');
+            var profile_image_id = $(this).attr('data-id');
+            $.ajax({
+                    url: baseUrl+'/ajax_image_remove',
+                    type:'POST',
+                    data: {
+                            "profile_image_id": profile_image_id,
+                            "_token": token,
+                    },
+                    success: function (data){
+                        $('#proimg_'+data).remove();
+                    }
+            });
+    });
+    $(document).on("click", '.uploaded-img' , function() {
+        var token = $('meta[name="csrf-token"]').attr('content');
+        var profile_id = $(this).attr('image-id');
+        $.ajax({
+            url: baseUrl+'/ajax_image_setprofile',
+            type:'POST',
+            data: {
+                    "profile_id": profile_id,
+                    "_token": token,
+            },
+            success: function (data){   
+                $("div").removeClass("img-preview-active");                         
+                $("div[profile-id='"+profile_id+"']").addClass('img-preview-active');
+            }
+        });
+    });
